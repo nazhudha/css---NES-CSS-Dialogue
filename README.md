66b358d73d37feca0c05b395aad9a594a488934c
nazhudha <57568434+nazhudha@users.noreply.github.com>
2022-03-28 00:48:41 +0100

    added music

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NESS CSS Pokemon Dialogue
 Fun, quirky use of NESS CSS Library to create some Pokemon Dialogue.
 
+Plays Sound! 
 [Code Pen Link](https://codepen.io/nazhudha/pen/xxpdEBq)
 
 [Live Demo](https://nazhudha.github.io/css---NES-CSS-Dialogue/)
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('test working')
+
+const audio = new Audio("src/eye-of-the-tiger.mp3");
+
+audio.volume = 0.1
+
+
+// function to Make Element Move
+
+// function growPokeball() {
+//     element = document.getElementById("nes-pokeball");
+//     // element.classList.toggle("growPokeball");
+//     element.style.transform = "rotate(180deg)"
+//   }
+
+//   function moveAsh() {
+//     element = document.getElementById("ash-run");
+//     // element.classList.toggle("growPokeball");
+//     element.style.transform = "rotate(180deg)"
+//   }
